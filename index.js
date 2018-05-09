63a77b80fb407246aa56391125fb55058db9ad0e
James Ferguson <ncjamesferguson@gmail.com>
2018-05-09 19:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  return drivers.filter(name)
+  return drivers.filter(function (driver) { driver.name === name} )
 }
 
 function fuzzyMatch(drivers, string) {
